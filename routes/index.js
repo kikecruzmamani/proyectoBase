4ef8ebb6dab8a6749b3d90b668a697c7ed3c5416
kikecruzmamani <kikelike05@gmail.com>
2016-11-29 20:22:12 -0400

    listo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var reclamo ={tipodereclamo:'odeco',
+              observacion:'mal servicio',
+              fechadereclamo:'29/11/2016'};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Proyecto final', subtitle: 'avances en computacion', reclamo:reclamo});
 });
 
 module.exports = router;
